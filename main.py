1f029966e3fabfebd77a6add1488bf298d0b48df
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-02-23 15:53:41 +0530

    staring

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 #Radhe Radhe
+#In this main.py file i will be making a dekstop notifier .
+#So copilot you gotta help me out in making it 
+
+
+import time
+from player import Player
+from notifier import Notifier
+from config import Config
+from logger import Logger
+from datetime import datetime
+from threading import Thread
+from os import path
+from sys import exit
+
